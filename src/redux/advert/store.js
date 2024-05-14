1a25ba1bcf65db0fb73e876168094f045cf1004f
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2024-05-14 20:27:30 +0300

    running update

---
+++ src/redux/advert/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { advertsReducer } from './advertSlice';
+import { filterReducer } from './filterSlice';
+
+export const store = configureStore({
+  reducer: {
+    adsverts: advertsReducer,
+    filter: filterReducer,
+  },
+});
