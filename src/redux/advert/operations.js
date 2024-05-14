e68ecfa73bfbd2099c77d0885e8e5de1a0d76eb5
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2024-05-14 18:08:19 +0300

    operations.js added

---
+++ src/redux/advert/operations.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://66393a1d4253a866a250c722.mockapi.io/'; // api :endpoint
+
+export const fetchAll = createAsyncThunk(
+  'advert/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('advert');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addAdvert = createAsyncThunk(
+  'advert/addAdvert',
+  async (newAdvert, thunkAPI) => {
+    try {
+      const response = await axios.post('advert', newAdvert);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteAdvert = createAsyncThunk(
+  'advert/deleteAdvert',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`advert/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
